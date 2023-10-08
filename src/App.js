c7e49653d9f842160a50473ab2ead9e3c11e07c7
Adrian <a.grochowski17@gmail.com>
2023-10-08 19:34:01 +0200

    Adding functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useState } from 'react';
+
 export default function App() {
+	const [inputTask, setInputTask] = useState('');
+	const [tasksList, setTasksList] = useState([]);
+
+	function handleAddTask(e) {
+		e.preventDefault();
+
+		if (!inputTask) return;
+
+		setTasksList((tasks) => [
+			...tasks,
+			{ id: Date.now(), inputTask, isDone: false },
+		]);
+
+		setInputTask('');
+	}
+
+	function handleDeleteTask(id) {
+		setTasksList((tasks) => tasks.filter((task) => task.id !== id));
+	}
+
+	function handleToggleTask(id) {
+		setTasksList((tasks) =>
+			tasks.map((task) =>
+				task.id === id ? { ...task, isDone: !task.isDone } : task
+			)
+		);
+	}
+
 	return (
 		<>
 			<Header />
-			<AddTask />
-			<ListTasks />
+			<AddTask
+				inputTask={inputTask}
+				onAddTask={handleAddTask}
+				setInputTask={setInputTask}
+			/>
+			{tasksList.length === 0 ? (
+				<h3 className='no-tasks-text'>Add your tasks</h3>
+			) : (
+				<ListTasks
+					tasksList={tasksList}
+					onDeleteTask={handleDeleteTask}
+					onToggleTask={handleToggleTask}
+				/>
+			)}
 		</>
 	);
 }
@@ -12,34 +54,50 @@ function Header() {
 	return <h1 className='heading'>Todo List</h1>;
 }
 
-function AddTask() {
+function AddTask({ inputTask, onAddTask, setInputTask }) {
 	return (
-		<form className='form-add-task'>
-			<input type='text' placeholder='Add task...' />
+		<form className='form-add-task' onSubmit={onAddTask}>
+			<input
+				type='text'
+				placeholder='Add task...'
+				value={inputTask}
+				onChange={(e) => setInputTask(e.target.value)}
+			/>
 			<Button>Add</Button>
 		</form>
 	);
 }
 
-function ListTasks() {
+function ListTasks({ tasksList, onDeleteTask, onToggleTask }) {
 	return (
 		<ul className='tasks-list'>
-			<Task />
-			<Task />
-			<Task />
-			<Task />
+			{tasksList.map((task) => (
+				<Task
+					task={task}
+					onDeleteTask={onDeleteTask}
+					onToggleTask={onToggleTask}
+					key={task.id}
+				/>
+			))}
 		</ul>
 	);
 }
 
-function Task() {
+function Task({ task, onDeleteTask, onToggleTask }) {
 	return (
 		<li className='task'>
 			<label>
-				<input type='checkbox' />
-				<span>Task</span>
+				<input
+					type='checkbox'
+					checked={task.isDone}
+					onChange={() => onToggleTask(task.id)}
+					style={task.isDone ? { accentColor: '#ff6d00' } : {}}
+				/>
+				<span className={task.isDone ? 'is-done' : ''}>{task.inputTask}</span>
 			</label>
-			<span className='delete-btn'>✖</span>
+			<span className='delete-btn' onClick={() => onDeleteTask(task.id)}>
+				✖
+			</span>
 		</li>
 	);
 }
